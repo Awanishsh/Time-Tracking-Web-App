b0a404e633dda428be3c7c49d5f4f074e7299bcd
Awanish Sharma <awanish.sharma.5683@gmail.com>
2025-07-30 21:22:16 +0530

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://time-tracking-web-app-backed-3.onrender.com/api/v1',
 });
 
 export const setAuthToken = (token) => {
